426d6b8095add648cf9b68a611629d83a77e0a2a
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2017-06-14 14:54:00 +0200

    Cookie cutter operational

--- src/01_pre_processing/cutter.py
+++ src/01_pre_processing/cutter.py
@@ -20,8 +20,8 @@ def timing(f):
         time1 = time.time()
         ret = f(*args, **kwargs)
         time2 = time.time()
-        print('%s function took %0.3f ms'.format(f.func_name,
-                                                 (time2 - time1) * 1000.0))
+        print('{} function took {} ms'.format(f.__name__,
+                                              (time2 - time1) * 1000.0))
         return ret
     return wrap
 
