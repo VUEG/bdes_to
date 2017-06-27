ffe52cb09010dc0336eb2e2df3ab1470298b8224
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2017-06-27 16:04:56 +0200

    Handle NoData

--- src/01_pre_processing/multi_warp.py
+++ src/01_pre_processing/multi_warp.py
@@ -23,7 +23,27 @@ def timing(f):
     return wrap
 
 
-def warp_raster(srcraster, outdir, verbose=False):
+def parse_nodata(dtype):
+    """
+    """
+
+    dtype_nodata = {
+        'Byte': 0,
+        'Uint8': 0,
+        'Int16': -32768,
+        'Uint16': 0,
+        'Int32': -2147483648,
+        'Uint32': 0,
+        'Float32': -3.40282e+38
+    }
+
+    if dtype not in list(dtype_nodata.keys()):
+        raise ValueError("Invalid dtype: {}".format(dtype))
+    else:
+        return dtype_nodata[dtype]
+
+
+def warp_raster(srcraster, outdir, dtype, verbose=False):
     """
     """
     logger = multiprocessing.get_logger()
@@ -31,16 +51,33 @@ def warp_raster(srcraster, outdir, verbose=False):
     dst_raster = os.path.join(outdir, os.path.basename(srcraster))
     dst_vrt_raster = dst_raster.replace(".tif", ".vrt")
 
+    if dtype == "Float32":
+        trsl_raster = dst_raster.split(".")[0] + "_tmp.tif"
+    else:
+        trsl_raster = dst_raster
+
     logger.info('Warping {} into {}'.format(srcraster, dst_raster))
+
+    nodata_value = parse_nodata(dtype)
 
     cmd_warp = ["gdalwarp", "-of", "vrt", srcraster, dst_vrt_raster,
                 "-te", "2000000.0 ", "1000000.0", "6526000.0", "5410000.0",
-                "-multi", "-dstnodata", "-3.40282e+38"]
+                "-multi", "-ot", dtype, "-dstnodata", str(nodata_value)]
     cmd_trsl = ["gdal_translate", "-co", "COMPRESS=DEFLATE",
-                dst_vrt_raster, dst_raster]
+                "-a_nodata", str(nodata_value), "-a_srs", "EPSG:3035",
+                "-ot", dtype, dst_vrt_raster, trsl_raster]
 
     warp_res = call(cmd_warp, shell=False)
     trsl_res = call(cmd_trsl, shell=False)
+
+    # Float32 rasters need additional processing to correct the NoData value.
+    if dtype == "Float32":
+        cmd_calc = ['rio', 'calc', '(where (== (take a 1) 0) -3.40282e+38 a)',
+                    '--name', 'a={}'.format(trsl_raster),
+                    '--co', 'compress=DEFLATE', dst_raster]
+        print(cmd_calc)
+        calc_res = call(cmd_calc, shell=False)
+        os.remove(trsl_raster)
 
     os.remove(dst_vrt_raster)
 
@@ -48,7 +85,7 @@ def warp_raster(srcraster, outdir, verbose=False):
 
 
 @timing
-def multi_warp(indir, outdir, cpus, logger=None, verbose=False):
+def multi_warp(indir, outdir, cpus, dtype, logger=None, verbose=False):
     """
     """
 
@@ -64,7 +101,7 @@ def multi_warp(indir, outdir, cpus, logger=None, verbose=False):
     pool = multiprocessing.Pool(processes=cpus)
     # Multitprocessing map only takes 1 argument, so we have to use
     # partials
-    pool.map(functools.partial(warp_raster, outdir=outdir),
+    pool.map(functools.partial(warp_raster, dtype=dtype, outdir=outdir),
              src_rasters)
     return(True)
 
@@ -73,8 +110,9 @@ def multi_warp(indir, outdir, cpus, logger=None, verbose=False):
 @click.option('-v', '--verbose', is_flag=True)
 @click.argument('indir', nargs=1, type=click.Path(exists=True))
 @click.argument('outdir', nargs=1)
+@click.option('-d', '--dtype', type=str, default="float32")
 @click.option('-c', '--cpus', type=int, default=1)
-def cli(indir, outdir, cpus, verbose):
+def cli(indir, outdir, dtype, cpus, verbose):
     """ Command-line interface."""
 
     if verbose:
@@ -100,8 +138,8 @@ def cli(indir, outdir, cpus, verbose):
 
     assert cpus > 0, 'ERROR: number of jobs must be a positive integer'
 
-    success = multi_warp(indir=indir, outdir=outdir,  cpus=cpus, logger=logger,
-                         verbose=verbose)
+    success = multi_warp(indir=indir, outdir=outdir,  dtype=dtype, cpus=cpus,
+                         logger=logger, verbose=verbose)
     if success:
         return(0)
     else:
