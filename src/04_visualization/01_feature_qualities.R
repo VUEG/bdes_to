56a36ce9773427317ada2ffe52e0e05c9088ade1
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2017-07-12 10:51:29 +0200

    Plotting code for diffs

--- src/04_visualization/01_feature_qualities.R
+++ src/04_visualization/01_feature_qualities.R
@@ -1,194 +1,198 @@
 library(dplyr)
+library(gganimate)
 library(ggplot2)
 library(hrbrthemes)
+library(purrr)
+library(RColorBrewer)
 library(readr)
 library(tidyr)
 library(zonator)
 library(viridis)
 
+zjapan_root <- "~/Dropbox/project.J-PriPA/japan-zsetup"
+
+source(file.path(zjapan_root, "R/00_lib/utils.R"))
+
 # Helper functions --------------------------------------------------------
 
-# panel.smooth function is built in.
-# panel.cor puts correlation in upper panels, size proportional to correlation
-panel.cor <- function(x, y, digits=2, prefix="", cex.cor, ...) {
-  usr <- par("usr"); on.exit(par(usr))
-  par(usr = c(0, 1, 0, 1))
-  r <- abs(cor(x, y))
-  txt <- format(c(r, 0.123456789), digits = digits)[1]
-  txt <- paste(prefix, txt, sep = "")
-  if (missing(cex.cor)) cex.cor <- 0.8/strwidth(txt)
-  text(0.5, 0.5, txt, cex = cex.cor * r)
+get_feature_ranges <- function() {
+  feature_ranges <- readr::read_csv("data/feature_ranges.csv") %>%
+    dplyr::slice(11:n()) %>% 
+    dplyr::select(feature_long = feature, dplyr::everything()) %>%
+    dplyr::mutate(feature = gsub("\\.tif$", "", basename(feature_long))) %>%
+    dplyr::select(feature, dplyr::everything(), -feature_long)
+  return(feature_ranges)
 }
 
+get_feature_rl <- function() {
+  feature_rl <- readr::read_csv("data/spp_rl_statuses.csv") %>%
+    dplyr::mutate(feature = tolower(gsub("\\s", "_", species))) %>%
+    dplyr::mutate(status = gsub("LR/nt", "NT", status)) %>%
+    dplyr::mutate(status = factor(status, levels = c("DD", "LC", "NT", "VU", "EN", "CR")))
+  return(feature_rl)
+}
 
-plot_pairs <- function(dat, taxon_name = NULL) {
-
-  if (is.null(taxon_name)) {
-    title <- "All taxa"
-    dat <- dat %>%
-      dplyr::select(log_count, log_mean_ol, morans_i, pr_rem)
-  } else {
-    title <- taxon_name
+get_feature_performance <- function(src_zproject, src_variant_id, 
+                                dst_variant_name, top, groups = NULL) {
+  feature_perf <- zonator::get_variant(src_zproject, src_variant_id) %>%
+    # Get results associated with the variant
+    zonator::results() %>%
+    # Get the performance data from results for the top fraction
+    zonator::performance(pr.lost = top) %>%
+    # Get rid of the original pr_lost
+    dplyr::select(-pr_lost) %>%
+    # Make feature / proportion remaining long
+    tidyr::gather(feature, pr_rem) %>% 
+    # Rename variant and add top fraction
+    dplyr::mutate(variant = dst_variant_name,
+                  fraction = paste0((1.0 - top) * 100, "%")) %>% 
+    # Join together with aux data
+    dplyr::left_join(get_feature_ranges()) %>%
+    dplyr::left_join(get_feature_rl()) %>%
+    # Calculate more stats
+    dplyr::mutate(log_count = log(count),
+                  log_mean_ol = log(mean_ol),
+                  count_rank = row_number(count),
+                  mean_ol_rank = row_number(-mean_ol))
+  if (is.null(groups)) {
+    # If no groups are provided, use 1 for everything
+    groups <- 1
   }
-  pairs(dat, lower.panel = panel.smooth, upper.panel = panel.cor,
-        pch = 20, main = title)
-
+  feature_perf$group <- groups
+  return(feature_perf)
 }
 
 # Load variants and configure groups --------------------------------------
 
-zproject <- zonator::load_zproject('analyses/zonation/priocomp/')
-# 04_abf_all_wgt
+# Load the project, creates japan_zproject. WARNING: loaded object is cached
+# using memoise-package. If results have change, don't load the cached version.
+# Also if the cache needs to updated, you will have to do this manually.
+# > key <- list("zsetup/bdes_to")
+# > saveCache(zproject, key = key)
+zproject <- .load_zproject("zsetup/bdes_to", cache = TRUE, debug = TRUE)
 
-# Get performance for top 5% and Drop first (pr_lost) and last (cost)
-# columns
-v04_top10 <- zonator::get_variant(zproject, 4) %>%
-  zonator::results() %>%
-  zonator::performance(pr.lost = 0.9) %>%
-  dplyr::select(-pr_lost, -pop_density_v5) %>%
-  tidyr::gather(feature, pr_rem) %>%
-  # Remove ES feautures
-  dplyr::slice(10:n()) %>%
-  dplyr::mutate(variant = "ALL")
-
-v12_top10 <- zonator::get_variant(zproject, 12) %>%
-  zonator::results() %>%
-  zonator::performance(pr.lost = 0.9) %>%
-  dplyr::select(-pr_lost, -pop_density_v5) %>%
-  tidyr::gather(feature, pr_rem) %>%
-  dplyr::mutate(variant = "BD")
-
-v21_top10 <- zonator::get_variant(zproject, 21) %>%
-  zonator::results() %>%
-  zonator::performance(pr.lost = 0.9) %>%
-  dplyr::select(-pr_lost, -pop_density_v5) %>%
-  tidyr::gather(feature, pr_rem) %>%
-  # Remove ES feautures
-  dplyr::mutate(variant = "BD_rank_ES")
-
-# Fix groups
 new_groups <- c(rep("Amphibians", 83), rep("Birds", 404), rep("Mammals", 164),
                 rep("Reptiles", 112))
 
-# Load auxiliary data -----------------------------------------------------
+# Process variants --------------------------------------------------------
 
-feature_ranges <- readr::read_csv("data/feature_ranges.csv") %>%
-  dplyr::select(feature_long = feature, dplyr::everything()) %>%
-  dplyr::mutate(feature = gsub("\\.tif$", "", basename(feature_long))) %>%
-  dplyr::select(feature, dplyr::everything(), -feature_long)
+# Variants are (sorted in logical order):
+# 01_abf_bio        - Baseline (BD only)
+#
+# 11_prl_biocar_bio - Priority ranking from carbon+BD, performance BD
+# 08_prl_car_bio    - Priority ranking from carbon, performance BD
+#
+# 12_prl_bioesc_bio - Priority ranking from ESs capacity+BD, performance BD
+# 09_prl_esc_bio    - Priority ranking from ESs capacity, performance BD
+#
+# 13_prl_bioesf_bio - Priority ranking from ESs flowzones+BD, performance BD
+# 10_prl_esf_bio    - Priority ranking from ESs flowzones, performance BD
 
-feature_autocor <- readr::read_csv("data/morans_I_values_772_features_2017-03-08_04-08-05.csv") %>%
-  dplyr::mutate(feature = gsub("\\.tif$", "", basename(feature)))
+# Get performance for top 10% and Drop first column (pr_lost)
 
-feature_rl <- readr::read_csv("data/spp_rl_statuses.csv") %>%
-  dplyr::mutate(feature = tolower(gsub("\\s", "_", species))) %>%
-  dplyr::mutate(status = gsub("LR/nt", "NT", status)) %>%
-  dplyr::mutate(status = factor(status, levels = c("DD", "LC", "NT", "VU", "EN", "CR")))
+top_fraction <- 0.90
+top_fractions <- seq(0.9, 0.1, -0.05)
 
-v04_feature_data <- v04_top10 %>%
-  dplyr::left_join(feature_ranges) %>%
-  dplyr::left_join(feature_autocor) %>%
-  dplyr::left_join(feature_rl) %>%
-  dplyr::mutate(log_count = log(count),
-                log_mean_ol = log(mean_ol),
-                count_rank = row_number(count),
-                mean_ol_rank = row_number(-mean_ol))
+all_feature_data <- list()
 
-v04_feature_data$group <- new_groups
+for (top_fraction in top_fractions) {
+  variant_ids <- c(11, 8, 12, 9, 13, 10, 1)
+  variant_names <- c("biocar", "car", "bioesc", "esc", "bioesf", "esf", "bio")
+  
+  feature_data <- purrr::map2(variant_ids, variant_names, 
+                              get_feature_performance, src_zproject = zproject, 
+                              top = top_fraction, groups = new_groups) %>% 
+    dplyr::bind_rows()
+  
+  feature_data$variant <- factor(feature_data$variant, levels = variant_names)
+  all_feature_data[[as.character(top_fraction)]] <- feature_data
+}
 
-v12_feature_data <- v12_top10 %>%
-  dplyr::left_join(feature_ranges) %>%
-  dplyr::left_join(feature_autocor) %>%
-  dplyr::left_join(feature_rl) %>%
-  dplyr::mutate(log_count = log(count),
-                log_mean_ol = log(mean_ol),
-                count_rank = row_number(count),
-                mean_ol_rank = row_number(-mean_ol))
+feature_data <- dplyr::bind_rows(all_feature_data)
 
-v12_feature_data$group <- new_groups
-
-v21_feature_data <- v21_top10 %>%
-  dplyr::left_join(feature_ranges) %>%
-  dplyr::left_join(feature_autocor) %>%
-  dplyr::left_join(feature_rl) %>%
-  dplyr::mutate(log_count = log(count),
-                log_mean_ol = log(mean_ol),
-                count_rank = row_number(count),
-                mean_ol_rank = row_number(-mean_ol))
-
-v21_feature_data$group <- new_groups
-
-all_feature_data <- dplyr::bind_rows(list(v12_feature_data, v04_feature_data,
-                                          v21_feature_data))
-all_feature_data$variant <- factor(all_feature_data$variant,
-                                   levels = c("BD", "ALL", "BD_rank_ES"))
-
-# Count differences between ranks in different variants
-variant_comp <- all_feature_data %>%
-  dplyr::select(feature, group, count_rank, log_count, variant, pr_rem) %>%
+# Count differences between ranks between the baseline (bio) and everything 
+# else
+variant_comp <- feature_data %>%
+  dplyr::select(feature, group, count_rank, log_count, count, variant, fraction, pr_rem) %>%
   tidyr::spread(variant, pr_rem) %>%
-  dplyr::mutate(first_diff = ALL - BD,
-                second_diff = BD_rank_ES - BD) %>%
-  dplyr::select(count_rank, log_count, group, first_diff, second_diff) %>%
-  tidyr::gather(diff, value, -count_rank, -log_count, -group) %>%
-  dplyr::mutate(diff = factor(diff, levels = c("first_diff", "second_diff"),
-                              labels = c("Between ALL and BD",
-                                         "Between BD_rank_ES and BD")))
+  dplyr::mutate(biocar_bio = biocar - bio,
+                car_bio = car - bio,
+                bioesc_bio = bioesc - bio,
+                esc_bio = esc - bio,
+                bioesf_bio = bioesf - bio,
+                esf_bio = esf - bio) %>%
+  dplyr::select(count_rank, log_count, group, fraction, count, biocar_bio:esf_bio) %>%
+  tidyr::gather(diff, value, -count_rank, -log_count, -count, -group, -fraction)
 
-variant_comp_rl <- all_feature_data %>%
+variant_comp_rl <- feature_data %>%
   dplyr::select(feature, status, count_rank, log_count, variant, pr_rem) %>%
   tidyr::spread(variant, pr_rem) %>%
-  dplyr::mutate(first_diff = ALL - BD,
-                second_diff = BD_rank_ES - BD) %>%
-  dplyr::select(count_rank, log_count, status, first_diff, second_diff) %>%
-  tidyr::gather(diff, value, -count_rank, -log_count, -status) %>%
-  dplyr::mutate(diff = factor(diff, levels = c("first_diff", "second_diff"),
-                              labels = c("Between ALL and BD",
-                                         "Between BD_rank_ES and BD")))
-
-# Plot cross-correlation matrix -------------------------------------------
-
-p1 <- plot_pairs(dat = all_feature_data)
-
-
-# Plot range size vs RL status --------------------------------------------
-
-p1_1 <- ggplot(dplyr::filter(all_feature_data, variant == "ALL"),
-               aes(x = status, y = log_count)) +
-  geom_boxplot(varwidth = TRUE) + theme_ipsum_rc()
+  dplyr::mutate(biocar_bio = biocar - bio,
+                car_bio = car - bio,
+                bioesc_bio = bioesc - bio,
+                esc_bio = esc - bio,
+                bioesf_bio = bioesf - bio,
+                esf_bio = esf - bio) %>%
+  dplyr::select(count_rank, log_count, status, biocar_bio:esf_bio) %>%
+  tidyr::gather(diff, value, -count_rank, -log_count, -status)
 
 # Plot orderd coverage ----------------------------------------------------
 
 # Order data by area
 
-p3 <- ggplot(all_feature_data, aes(x = count_rank, y = pr_rem)) +
-  geom_point(alpha = 0.1) + geom_smooth() + ylab("Proportion of range covered") +
-  facet_wrap(~ variant, nrow = 1, ncol = 3) +
+p1 <- ggplot(feature_data, aes(x = count_rank, y = pr_rem, frame = fraction)) +
+  geom_point(alpha = 0.1) + geom_smooth(aes(group = fraction)) + 
+  ylab("Proportion of range covered") +
+  facet_wrap(~ variant, nrow = 4, ncol = 2) +
   ylim(c(0, 1)) + scale_x_discrete("Range rank (from smallest to largest)",
                                    limits = c(1, 763)) +
   theme_ipsum_rc()
 
-ggsave("reports/figures/figureExtra/BD_coverage.png", p3,
-       width = 10, height = 4)
+gganimate(p1, ani.width = 1000, ani.height = 800, 
+          filename = "reports/figures/diff_ordered_by_rank.gif")
+
+#ggsave("reports/figures/diff_ordered_by_rank.png", p1, width = 6, height = 8)
+
+
 
 # Plot diffs --------------------------------------------------------------
 
-p4 <- ggplot(variant_comp, aes(x = log_count, y = value, color = diff)) +
-  geom_point(alpha = 0.5, size = 0.5) + geom_smooth(size = 0.5) +
-  scale_color_viridis(discrete = TRUE, end = 0.7) +
+p2 <- ggplot(variant_comp, aes(x = count, y = value, frame = fraction)) +
+  geom_point(alpha = 0.5, size = 0.5) + geom_smooth(aes(group = fraction), size = 0.7) +
+  scale_color_brewer(type = "qual", palette = "Dark2") +
   scale_y_continuous(breaks = seq(-1, 0.25, 0.25),
                      labels = paste0(seq(-1, 0.25, 0.25) * 100, "%")) +
   ylab("Difference in proportion of range covered") +
-  xlab("log(range size)") +
+  xlab("log(range size)") + facet_wrap(~ diff) + 
   theme_ipsum_rc() + theme(legend.position = "top", legend.title = element_blank())
 
 # Wrap per group
-p5 <- p4 + facet_wrap(~ group)
+#p3 <- p2 + facet_wrap(~ group)
+
+gganimate(p2, ani.width = 1000, ani.height = 800,
+          filename = "reports/figures/diff_ordered_by_area.gif")
+
+p3 <- ggplot(variant_comp, aes(x = log_count, y = value, frame = fraction)) +
+  geom_point(alpha = 0.5, size = 0.5) + geom_smooth(aes(group = fraction), size = 0.7) +
+  scale_color_brewer(type = "qual", palette = "Dark2") +
+  scale_y_continuous(breaks = seq(-1, 0.25, 0.25),
+                     labels = paste0(seq(-1, 0.25, 0.25) * 100, "%")) +
+  ylab("Difference in proportion of range covered") +
+  xlab("log(range size)") + facet_wrap(~ diff) + 
+  theme_ipsum_rc() + theme(legend.position = "top", legend.title = element_blank())
+
+gganimate(p3, ani.width = 1000, ani.height = 800,
+          filename = "reports/figures/diff_ordered_by_log_area.gif")
+
+# ggsave("reports/figures/diff_ordered_by_area.png", p2,
+#        width = 6, height = 6)
+# ggsave("reports/figures/diff_ordered_by_area_group.png", p3,
+#        width = 8, height = 6)
 
 # Per red-list
-p6 <- ggplot(variant_comp_rl, aes(x = log_count, y = value, color = diff)) +
+p4 <- ggplot(variant_comp_rl, aes(x = log_count, y = value, color = diff)) +
   geom_point(alpha = 0.5, size = 0.5) + geom_smooth(size = 0.5) +
-  scale_color_viridis(discrete = TRUE, end = 0.7) +
+  scale_color_brewer(type = "qual", palette = "Dark2") +
   scale_y_continuous(breaks = seq(-1, 0.25, 0.25),
                      labels = paste0(seq(-1, 0.25, 0.25) * 100, "%")) +
   ylab("Difference in proportion of range covered") +
@@ -196,10 +200,8 @@ p6 <- ggplot(variant_comp_rl, aes(x = log_count, y = value, color = diff)) +
   theme_ipsum_rc() + theme(legend.position = "top", legend.title = element_blank())
 
 # Wrap per group
-p7 <- p6 + facet_wrap(~ status)
+p5 <- p4 + facet_wrap(~ status)
 
-ggsave("reports/figures/figureExtra/BD_coverage_diff.png", p4,
-       width = 6, height = 6)
-ggsave("reports/figures/figureExtra/BD_coverage_diff_group.png", p5,
-       width = 7, height = 7)
+ggsave("reports/figures/diff_ordered_by_area_rlstatus.png", p5,
+       width = 10, height = 8)
 
